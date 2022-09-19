bb8ed704293fd08d912908850865337c54a69ce2
FabianMontossi <110575173+FabianMontossi@users.noreply.github.com>
2022-09-19 17:00:24 -0300

    Filter, sort missing

--- js/products.js
+++ js/products.js
@@ -10,7 +10,9 @@ function AssignCatID(){
 
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/" + AssignCatID() + ".json";
 const container = document.getElementById("productsContainer");
-
+const sortBySoldAmount = document.getElementById("sortBySoldAmount");
+const sortAsc = document.getElementById("sortAsc");
+const sortDesc = document.getElementById("sortDesc");
 
 function ChangeProdTitles(catName) {
     document.querySelector(".lead").innerHTML = `Verás aquí todos los ${catName} del sitio`;
@@ -32,119 +34,93 @@ function addProduct(){
                 <p class="mb-1">${item.description}</p>
             </div>
         </div>
-    </div>`
+    </div>`;
 }
 
-let arrItems = [];
+const arrItems = [];
 let maxCost = 0;
 
-function showProducts(array){
+function showProducts(array, addToArray){
     for (item of array){
-        // Pusheamos esto al array arrItems, asi luego podemos filtrar los productos
-        arrItems.push(item);
+        
+        // Pusheamos cada producto al array arrItems, solo al hacer el fetch
+        if (addToArray){
+            arrItems.push(item);
+        }
 
         if (parseInt(item.cost) > parseInt(maxCost)){
             maxCost = item.cost;
-            console.log(maxCost);
         }
         
-        container.innerHTML += 
-            `<div class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${item.image}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${item.name} - USD${item.cost}</h4>
-                            <small class="text-muted">${item.soldCount} vendidos</small>
-                        </div>
-                        <p class="mb-1">${item.description}</p>
-                    </div>
-                </div>
-            </div>`
+        addProduct();
     }
 }
 
 async function fetchProducts(param){
 
-    let response = await fetch(param);
+    const response = await fetch(param);
     
     if (response.ok){
-        let responseContents = await response.json();
+        const responseContents = await response.json();
         ChangeProdTitles(responseContents.catName);
-        showProducts(responseContents.products);
+        showProducts(responseContents.products, true);
     } else{
         alert("HTTP Error: " + response.code, response.status);
     }
 }
 
-
-
-function returnMin(){
-    let minPriceField = document.getElementById("rangeFilterPriceMin").value;
+function minPrice(){
+    const minPriceField = document.getElementById("rangeFilterPriceMin").value;
     let minPrice = 0;
-    console.log("minPrice", minPrice);
     
     // si está vacio devolvemos 0, si está vacio devolvemos el valor escrito
-    if (!(minPriceField === "" || minPriceField === null || minPriceField === undefined))
+    if (!(minPriceField === "" || minPriceField === null || minPriceField === undefined)){
         minPrice = minPriceField;
+    }
 
-        console.log("minPrice SEGUNDO", minPrice);
     return parseInt(minPrice);
 }
 
-function returnMax(mostExpensiveItem){
-    let maxPriceField = document.getElementById("rangeFilterPriceMax").value;
-    let maxPrice = mostExpensiveItem;
+function maxPrice(mostExpensiveItem){
+    const maxPriceField = document.getElementById("rangeFilterPriceMax").value;
     
     // Recorrerlo el array antes de llamar a esta funcion para ver cual es el min y el max
-    // si está vacio devolvemos el valor del item mas caro, si está vacio devolvemos el valor escrito
-    if (!(maxPriceField === "" || maxPriceField === null || maxPriceField === undefined))
-        maxPrice = maxPriceField;
-
-    console.log("max", maxPrice, "maxPriceField", maxPriceField);
-    return parseInt(maxPrice);
-}
-
-
-/*
-function filterProducts(product){
-
-    // Si el MIN ingresado es => al costo && el MAX ingresado es menor al costo, then...
-    
-    if ((returnMin() >= product.cost) && (returnMax(maxCost) <= product.cost)){
-        
+    // si está vacio devolvemos el valor del item mas caro, SI NO, devolvemos el valor escrito
+    if (!(maxPriceField === "" || maxPriceField === null || maxPriceField === undefined)){
+        return maxPriceField;
     }
-    return (product);
+
+    return parseInt(mostExpensiveItem);
 }
-*/
 
+function filterProducts(){
+    container.innerHTML = "";
+    const filteredProds = arrItems.filter(function(product){
+        // Si el MIN ingresado es => al costo && el MAX ingresado es menor al costo
+        if ((product.cost >= minPrice()) && (product.cost <= maxPrice(maxCost))){
+            return product;
+        }
+    });
+    
+    showProducts(filteredProds, false);
 
-// llamamos a la funcion fetchProducts
+    return filteredProds;
+}
+
 fetchProducts(DATA_URL);
 
 document.getElementById("rangeFilterPrice").addEventListener("click", function(){
-    //console.log("asd", maxCost);
-    container.innerHTML = "";
-    showProducts(arrItems.filter(function(product){
-        //console.log("HERE",  parseInt(product.cost), product.cost.toString());
-        // Si el MIN ingresado es => al costo && el MAX ingresado es menor al costo, then...
-        console.log((returnMin() >= parseInt(product.cost)));
-        //if ((parseInt(product.cost) >= returnMin()) && ((product.cost) <= returnMax(maxCost))){
-            if ((product.cost >= returnMin()) && ((product.cost <= 13600))){
-            console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            return (product);
-            //console.log("minPrice", returnMin(), "max", returnMax(), "product.cost", product.cost);
-
-            
-        }
-        
-    }));
-
+    filterProducts(false);
 });
 
+document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("rangeFilterPriceMin").value = "";
+    document.getElementById("rangeFilterPriceMax").value = "";
+    filterProducts(false);
+});
 
-// filtrar solo si uno de los dos (min o max) tienen algo !== a "" o null
-// IF ITEM.price >= minimo, y ITEM.price <= maximo
-// onClear, volve a mostrar todo lo que mostrabas antes
+sortBySoldAmount.addEventListener("click", function(){
+    filterProducts();
+});
+
+//points.sort(function(a, b){return a - b});
