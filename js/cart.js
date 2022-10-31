553780930db7b29efb861972954cb5afbb13c43c
FabianMontossi <110575173+FabianMontossi@users.noreply.github.com>
2022-10-30 23:45:25 -0300

    Casi terminado...

--- js/cart.js
+++ js/cart.js
@@ -4,69 +4,17 @@ function getUserId(){
 
 const CART_URL = CART_INFO_URL + getUserId() + EXT_TYPE;
 const content = document.getElementById("container");
-const inptCount = document.getElementById("inptCount");
 let tdSubtotal = document.getElementById("tdSubtotal");
 let subtotal = document.getElementById("subtotal");
 
 
-function asd(){
-   /* console.log(tdSubtotal)
-    let paragraph = document.createElement("p");
-    
-    let asd = document.createTextNode("HOLA");
-    
-    paragraph.appendChild(asd);
-    console.log("inptCount", inptCount)
-    paragraph.setAttribute("id", "subtotal");
-        console.log("tdSubtotal", tdSubtotal, "paragraph", paragraph, "subtotal", subtotal)
-        tdSubtotal.appendChild(paragraph);*/
-
-        tdSubtotal.addEventListener("load", function(){
-            inptCount.addEventListener("input", function(){
-                tdSubtotal.innerHTML += `<p id="subtotal">${calculateSubtotal(articles[i].unitCost, articles[i].count)}`;
-            });
-           /* <p id="subtotal">${calculateSubtotal(articles[i].unitCost, articles[i].count)}
-
-            ${calculateSubtotal(articles[i].unitCost, inptCount.value)}*/
+/*function subtotal(){
+    tdSubtotal.addEventListener("load", function(){
+        inptCount.addEventListener("input", function(){
+            tdSubtotal.innerHTML += `<p id="subtotal">${calculateSubtotal(articles[i].unitCost, articles[i].count)}`;
         });
-    }
-
-
-function showCart(products){
-    content.innerHTML += `
-        <h2 class="h2Cart">Carrito de Compras</h2>
-        <p class="headingsCart">Articulos a comprar</p>
-        <table>
-            <tr>
-                <th></th>
-                <th>Nombre</th>
-                <th>Costo</th>
-                <th>Cantidad</th>
-                <th>Subtotal</th>
-            </tr>
-            ${showProducts(products.articles)}
-        </table>
-        <hr class="hrCart">
-        ${showShipping()}
-        `;
-}
-
-function calculateSubtotal(unitCost, quantity){
-    
-
-   /* function function1() {
-        var ul = document.getElementById("list");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode("Four"));
-        ul.appendChild(li);
-      }*/
-
-    // ver tema de currency
-    // first time inptCount.value = articles[i].count
-    // unitCost * inptCount.value, onChange
-    return parseInt(unitCost) * parseInt(quantity);
-}
-
+    });
+}*/
 
 function showShipping(){
     let htmlContent = `
@@ -101,12 +49,37 @@ function showProducts(articles){
                 <td><p>${articles[i].name}</p></td>
                 <td><p>${articles[i].unitCost}</p></td>
                 <td><input id="inptCount" class="inputTxtCart" value="${articles[i].count}"></td>
-                <td id="tdSubtotal"><p>${articles[i].currency}</p> <p id="subtotal">${calculateSubtotal(articles[i].unitCost, articles[i].count)}</td>
+                <td id="tdSubtotal"><p>${articles[i].currency}</p> <p id="subtotal">${calculateSubtotal(articles[i])}</td>
             </tr>
         </tbody>
         `;
-    }console.log("DONE")
+    }
     return products;
+}
+
+function showCart(products){
+    content.innerHTML += `
+        <h2 class="h2Cart">Carrito de Compras</h2>
+        <p class="headingsCart">Articulos a comprar</p>
+        <table>
+            <tr>
+                <th></th>
+                <th>Nombre</th>
+                <th>Costo</th>
+                <th>Cantidad</th>
+                <th>Subtotal</th>
+            </tr>
+            ${showProducts(products.articles)}
+        </table>
+        <hr class="hrCart">
+        ${showShipping()}
+        `;
+}
+
+function calculateSubtotal(article){
+    //pendiente lo del currency
+    //quantity = document.getElementById("inptCount").value;
+    return parseInt(article.unitCost) * parseInt(article.count);
 }
 
 async function fetchCart(param){
@@ -117,10 +90,8 @@ async function fetchCart(param){
         const responseContents = await response.json();
         showCart(responseContents);
     } else{
-        alert("Unfortunately, there's something wrong :("); //  !!!!!!!!!!! MISMO QUE EN AssignCatId()
+        alert("Unfortunately, there's something wrong :("); // MISMO QUE EN AssignCatId()
     }
 }
-console.log("asdddd")
 fetchCart(CART_URL);
-console.log("asddd222222d")
 
